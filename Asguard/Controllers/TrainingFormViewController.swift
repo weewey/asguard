efefc24acc866016ae45de4682bfee2111a7dbbb
Yew Wee <chuayewwee@gmail.com>
2018-02-04 21:44:16 +0800

    Add training log form

--- Asguard/Controllers/TrainingFormViewController.swift
+++ Asguard/Controllers/TrainingFormViewController.swift
@@ -12,19 +12,36 @@ import Eureka
 class TrainingFormViewController : FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        //TODO: RUN SECTION, DATE, DISTANCE, PACE, DIFFICULTY, COMMENTS
+        let todayDate = Date()
         form +++ Section("Run")
             <<< DateRow(){
                 $0.title = "Date"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+                $0.value = todayDate
             }
-            <<< TextRow(){ row in
-                row.title = "D"
-                row.placeholder = "Enter text here"
+            <<< DecimalRow(){ row in
+                row.title = "Distance (KM)"
+                row.placeholder = "Distance of run"
             }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
+            <<< DecimalRow(){ row in
+                row.title = "Average Run Pace (min/km)"
+                row.placeholder = "5"
             }
+            <<< ActionSheetRow<String>() {
+                $0.title = "Type of Run"
+                $0.selectorTitle = "Pick the type of run"
+                $0.options = ["Easy Run","Tempo Run","Intervals","Long Run"]
+                $0.value = "Easy Run"
+            }
+            <<< TextAreaRow() { row in
+                row.title = "Comments"
+                row.placeholder = "Comments"
+            }
+            <<< ButtonRow() { row in
+                row.title = "Submit"
+                }.onCellSelection({ (cell, row) in
+                    print("Form Submitted!")
+                })
         navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
         animateScroll = true
         rowKeyboardSpacing = 20
